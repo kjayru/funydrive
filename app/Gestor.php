762635cbe1cfba8e5668c40fb40db73131300385
kjayru <wiltinoco@gmail.com>
2018-10-22 17:43:42 -0500

    de

--- app/Gestor.php
+++ app/Gestor.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Gestor extends Model
 {
     public static function conexion(){
-        $con = mysqli_connect("35.195.132.92", "wavydrive", "*WavyDrive2018*", "funydrive");
-
+        $mysqli = new mysqli("35.195.132.92", "wavydrive", "*WavyDrive2018*", "funydrive");
+        return $mysqli;
         
     }
 }
