fb5b8e075a9c51cc54b4432ab5039520f87f61f3
kjayru <wiltinoco@gmail.com>
2018-10-22 17:56:45 -0500

    ds

--- app/Gestor.php
+++ app/Gestor.php
@@ -12,7 +12,7 @@ class Gestor extends Model
         $select="Select * from usuario";
         $result = $con->query($select);
         while($row  = $result->fetch_assoc()){
-            $row['email']."<br>";
+           echo  $row['email']."<br>";
         }
        
         
