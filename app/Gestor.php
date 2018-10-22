98c8c36e48677f7d9da1b95a762be4716125ef09
kjayru <wiltinoco@gmail.com>
2018-10-22 17:47:49 -0500

    ds

--- app/Gestor.php
+++ app/Gestor.php
@@ -9,7 +9,7 @@ class Gestor extends Model
     public static function conexion(){
         $con = mysqli_connect("35.195.132.92", "wavydrive", "*WavyDrive2018*", "funydrive");
         
-        $select="select * from usuarios";
+        $select="show tables";
         $result = $con->query($select);
   
         dd($result);
